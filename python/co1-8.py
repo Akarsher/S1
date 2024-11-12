4a4fdf40cd2c86acbe1495d8cd158e6b9f77a295
Adithya S Nair <adithyasnair25@gmail.com>
2024-11-12 18:26:06 +0530

    CO1-8 added

---
+++ python/co1-8.py
@@ -0,0 +1,8 @@
+#8.	Get a string from an input string where all occurrences of first character replaced with ‘$’, except first character.
+
+l=input("Enter a String : ")
+f=l[0]
+
+l1=l[1:].replace(f,'$')
+
+print("New String : ",f+l1)
