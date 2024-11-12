f76d8d9a8ada32a02e32eba82d9f5050b3f9a27a
Akhil Mohanan <akhilmohnn@outlook.com>
2024-11-12 19:47:30 +0530

    co2-9 : printing right faced triangle-star pattern'

---
+++ python/co2-9.py
@@ -0,0 +1,13 @@
+"""To print triangle star pattern"""
+
+n = int(input("Enter the size: "))
+
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+
+for i in range(n):
+    for j in range(n-i-1):
+        print("*", end=" ")
+    print()
